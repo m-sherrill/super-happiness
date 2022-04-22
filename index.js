720e177a28e71035d7fe58eaff865c08bb2be480
Morgan Sherrill <mo.sherrill@outlook.com>
2022-04-21 22:32:27 -0700

    messing around setting up a fake wallet on solana

---
+++ index.js
@@ -0,0 +1,27 @@
+const {
+    Connection,
+    PublicKey,
+    clusterApiUrl,
+    Keypair,
+    LAMPORTS_PER_SOL
+} = require('@solana/web3.js')
+
+const wallet = new Keypair()
+
+const publicKey = new PublicKey(wallet._keypair.publicKey)
+
+const getWalletBalance = async() => {
+    try{
+        const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
+        const walletBalance = await connection.getBalance(publicKey)
+        console.log(walletBalance)
+    } catch(err) {
+        console.error(err)
+    }
+}
+
+const main = async() => {
+    await getWalletBalance()
+}
+
+main()
