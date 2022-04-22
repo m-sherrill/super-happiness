0d61083e3bf680bba63f45557990dee1aaa7c268
Morgan Sherrill <mo.sherrill@outlook.com>
2022-04-22 00:24:19 -0700

    added a function to airdrop test sol into my test wallet

--- index.js
+++ index.js
@@ -14,13 +14,24 @@ const getWalletBalance = async() => {
     try{
         const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
         const walletBalance = await connection.getBalance(publicKey)
-        console.log(walletBalance)
+        console.log(walletBalance/LAMPORTS_PER_SOL)
     } catch(err) {
         console.error(err)
     }
 }
 
+const airDropSol = async() => {
+    try{
+        const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
+        const fromAirDropSignature = await connection.requestAirdrop(publicKey, 2 * LAMPORTS_PER_SOL)
+        await connection.confirmTransaction(fromAirDropSignature)
+    } catch(err) {
+        console.error(err)
+    }
+}
 const main = async() => {
+    await getWalletBalance()
+    await airDropSol()
     await getWalletBalance()
 }
 
